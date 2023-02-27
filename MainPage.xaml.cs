c58aed4b50c12cb7b31f4fa03133b24794e9329a
Home <Home@LAPTOP-2E6V6ACM>
2023-02-28 00:52:32 +0200

    added grid and border.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,23 +2,30 @@
 
 using BindingToObject.Models;
 
+
 namespace Collections;
 
 public partial class MainPage : ContentPage
 {
 
 	public Student Student { get; set; }
+	
 
 	public MainPage()
 	{
-
+		
+		Student = new () { Name="Roni", Image="roni.jpg", BirthDate=new DateTime(2006,2,19)};
 		InitializeComponent();
-
+		this.BindingContext = Student;
+		
+		
 
 	}
+
+	
 }
 
-	
 
-	
+
+
 
